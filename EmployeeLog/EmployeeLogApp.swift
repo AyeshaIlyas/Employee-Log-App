6e29515f60b32c53e44de68c718c84563afefd8d
Ayesha Ilyas <annie16950@gmail.com>
2022-03-14 19:14:27 -0400

    Initial Commit

---
+++ EmployeeLog/EmployeeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeLogApp.swift
+//  EmployeeLog
+//
+//  Created by Ayesha Ilyas on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
